8ba1d33690679b02b0b405840f99732db7f05f96
madouh <tin.tin.nagi@gmail.com>
2015-09-08 03:38:11 +0200

    with facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger=Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, "807837356004287", "2164bbda0c19dcff1e6b7eae7a5a51b5"
            
 end
